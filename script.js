2493da007a362943d7fc6d5372e010a41d18c495
Ruan Lima <az.88004607@gmail.com>
2023-10-26 21:25:43 -0300

    Adição da função verificarcpf, e criação do dicionário clientes

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+var clientes = [
+    {
+        '123.456.789-00': {
+            nome: 'João Silva',
+            telefone: '(11) 12345-6789',
+            email: 'joao.silva@example.com',
+            dataDeNascimento: '01/01/1980'
+        }
+    },
+    {
+        '987.654.321-00': {
+            nome: 'Maria Santos',
+            telefone: '(21) 98765-4321',
+            email: 'maria.santos@example.com',
+            dataDeNascimento: '02/02/1990'
+        }
+    }
+];
+
+function verificarCPF() {
+    var cpf = document.getElementById('cpf').value;
+    var existe = false;
+
+    for (var i = 0; i < clientes.length; i++) {
+        if (clientes[i][cpf]) {
+            existe = true;
+            break;
+        }
+    }
+
+    if (existe) {
+        // Redireciona para a página se o CPF já existir
+        window.location.href = 'pagina_existente.html';
+    } else {
+        // Redireciona para outra página se o CPF não existir
+        window.location.href = 'pagina_nao_existente.html';
+    }
+}
+
+window.onload = function() {
+    var botao = document.getElementById('botao');
+    botao.addEventListener('click', verificarCPF);
+};
+
+
+
+
+
+
+
+
 function formatarCPF() {
     var cpf = document.getElementById('cpf');
     cpf.addEventListener('keydown', function(e) {
